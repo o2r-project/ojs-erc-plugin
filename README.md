d5c25b2fb88b0ad194ea0236d51bf1413daaf62e
tnier01 <t_nier01@uni-muenster.de>
2021-06-04 14:28:12 +0200

    deleted unused code

--- README.md
+++ README.md
@@ -4,69 +4,6 @@ OJS-ERC-Plugin
 
 work in progress 
 
-<!---
-**Content**
-* [Abstract](https://github.com/tnier01/geoOJS#abstract)
-* [Download & Installation](https://github.com/tnier01/geoOJS#download--installation)
-* [Specify geonames username ](https://github.com/tnier01/geoOJS#specify-geonames-username)  
-* [Contribute](https://github.com/tnier01/geoOJS#contribute)  
-* [Wiki](https://github.com/tnier01/geoOJS/wiki)  
-
-# Abstract
-*geoOJS* offers a novel way for authors to provide spatial properties of research works when submitting an article to a journal based on the open source software Open Journal Systems (OJS, https://pkp.sfu.ca/ojs/)
-
-The plugin adds the functionality, that the author can specify the articles content in terms of geospatial metadata during the submission process for submitting an article ("3. Enter Metadata"). 
-
-<div style="text-align:center">
-<img src="screenshots/SubmissionView.png" alt="Alt-Text" title="Screenshot of geoOJS: geospatial properties in the OJS submission process" width="50%" align="middle"/>
-
-*Screenshot of geoOJS: geospatial properties in the OJS submission process*
-</div>
-
-
-Authors can either search for a location and accept the suggested bounding box or manually create one or more suitable geometric shape(s) on a map. If authors enter geometries, a gazetteer is used to suggest a matching administrative unit’s name to the author. This allows geoOJS to store geospatial data in two forms: as text, using an administrative unit or standardised geographical norm data, and as geospatial coordinates in GeoJSON format. <br>
-Thereby the coordinates are stored accurately, while at the same time a textual description is accessible and flexible for non-map-related usage. In addition to displaying geospatial information on maps, it is also added to the HTML source code of article’s landing pages in a semantically meaningful way. <br> 
-In the article view, the properties specified by the author are then displayed and available for download as geoJSON. 
-
-<div style="text-align:center">
-<img src="screenshots/ArticleView.png" alt="Alt-Text" title="Screenshot of geoOJS: geospatial properties in the OJS article view" width="50%" align="middle"/>
-
-*Screenshot of geoOJS: geospatial properties in the OJS article view*
-</div>
-
-The plugin was developed as part of a bachelor thesis. This thesis can be found here: http://nbn-resolving.de/urn:nbn:de:hbz:6-69029469735. In addition, the plugin was presented at The Munin Conference on Scholarly Publishing 2020. Abstract, recording and slides can be found here: https://doi.org/10.7557/5.5590 or on youtube: https://youtu.be/-Lc9AjHq_AY.
-
-
-# Download & Installation 
-- to use the plugin, OJS must be running locally (further information you can find here: https://github.com/pkp/ojs)
-- download the plugin here as ```geoOJS.zip```
-- rename the unzipped folder ```geoOJS-master``` to ```geoOJS```
-- insert the folder ```geoOJS``` in the path ```ojs/plugins/generic/``` in OJS 
-- activate the plugin geoOJS in the OJS plug-in settings 
-- Specify your geonames username
-- If you want to disable CDN you can do it [here](https://github.com/tnier01/geoOJS/wiki/Disable-CDN)
-
-# Specify geonames username 
-- You have to specify your username for the geonames api, so that an alignment for the administrative units is possible. 
-- Create an account on https://www.geonames.org/login 
-- Enter the username in the geoOJS plug-in settings (OJS -> Settings -> Website -> Plugins -> Installed Plugins -> geoOJS -> blue arrow -> Settings)!
-
-<div style="text-align:center">
-<img src="screenshots/PluginSettings.png" alt="Alt-Text" title="Screenshot of geoOJS: plug-in settings" width="50%" align="middle"/>
-
-*Screenshot of geoOJS: plugin settings*
-</div>
-
-# Wiki 
-Further information you find in the [Wiki](https://github.com/tnier01/geoOJS/wiki). 
-
-**Wiki Content**
-* [geoJSON-Specification](https://github.com/tnier01/geoOJS/wiki/geoJSON-Specification)
-* [Disable CDN](https://github.com/tnier01/geoOJS/wiki/Disable-CDN)
-
-
---->
-
 # Contribute
 
 All help is welcome: asking questions, providing documentation, testing, or even development.
--- ojsErcPlugin.inc.php
+++ ojsErcPlugin.inc.php
@@ -20,7 +20,6 @@ class ojsErcPlugin extends GenericPlugin
 		// important to check if plugin is enabled before registering the hook, cause otherwise plugin will always run no matter enabled or disabled! 
 		if ($success && $this->getEnabled()) {
 
-  
 			/*
 			Load the current build of the o2r api, if it is not already there 
 			*/
@@ -53,7 +52,6 @@ class ojsErcPlugin extends GenericPlugin
 				else {
 		   	 	//echo "File downloading failed.";
 				}
-
 				
 				/*
 				Update the config.js of the build in terms of the baseURL specified in the plugin settings and set the ojsView to true. 
@@ -88,7 +86,7 @@ class ojsErcPlugin extends GenericPlugin
 				fclose($rawConfigFile);
 
 				/*
-				Update the index.html of the build in terms of the users path for the css-, js-, log-, and manifest-file(s). 
+				Update the index.html of the build in terms of the users path for the css-, js-, logo-, and manifest-file(s). 
 				*/
 				$request = Application::get()->getRequest();
 				$baseUrl = $request->getBaseUrl();
@@ -138,98 +136,13 @@ class ojsErcPlugin extends GenericPlugin
 
 			// Hooks for changing the frontent Submit an Article 3. Enter Metadata 
 			HookRegistry::register('Templates::Submission::SubmissionMetadataForm::AdditionalMetadata', array($this, 'extendSubmissionMetadataFormTemplate'));
-
-			// Hooks for changing the Metadata right before Schedule for Publication (not working yet)
-			//HookRegistry::register('Form::config::before', array($this, 'extendScheduleForPublication'));
-			//HookRegistry::register('Template::Workflow::Publication', array($this, 'extendScheduleForPublication2'));
-
-			// Hook for changing the article page 
-			HookRegistry::register('Templates::Article::Main', array(&$this, 'extendArticleMainTemplate'));
-			//HookRegistry::register('Templates::Article::Details', array(&$this, 'extendArticleDetailsTemplate'));
-			// Templates::Article::Main 
-			// Templates::Article::Details
-			// Templates::Article::Footer::PageFooter
-
+		
 			// Hook for creating and setting a new field in the database 
 			HookRegistry::register('Schema::get::publication', array($this, 'addToSchema'));
 			HookRegistry::register('Publication::edit', array($this, 'editPublication')); // Take care, hook is called twice, first during Submission Workflow and also before Schedule for Publication in the Review Workflow!!!
 
-			HookRegistry::register('ArticleHandler::view::galley', array($this, 'articleViewCallback'), HOOK_SEQUENCE_LATE);
-			HookRegistry::register('ArticleHandler::download', array($this, 'articleDownloadCallback'), HOOK_SEQUENCE_LATE);
-
-
 			$request = Application::get()->getRequest();
 			$templateMgr = TemplateManager::getManager($request);
-
-			
-
-			/*
-			In previous OJS versions, there was an option in the config.inc.php to enable or disable CDN. 
-			As it was deprecated in OJS 3.3, we decide it to integrate it by a plugin setting: checkboxDisableCDN. 
-			If the checkbox is checked the variable $checkboxDisableCDN is = "on" , CDN is disabled. Thus the plugins should not load any scripts or styles from a third-party website.
-			Old configuration option which is disabled: Config::getVar('general', 'enable_cdn'). 
-			
-			$request = Application::get()->getRequest();
-			$context = $request->getContext();
-			$checkboxDisableCDN = $this->getSetting($context->getId(), 'checkboxDisableCDN');
-
-			if ($checkboxDisableCDN !== "on") {
-				// if the user allows CDN 
-				$urlLeafletCSS = 'https://unpkg.com/leaflet@1.6.0/dist/leaflet.css';
-				$urlLeafletJS = 'https://unpkg.com/leaflet@1.6.0/dist/leaflet.js';
-				$urlLeafletDrawCSS = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw.css';
-				$urlLeafletDrawJS = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw.js';
-				// $urlJqueryJS = 'https://code.jquery.com/jquery-3.2.1.js';
-				// jquery no need to load, already loaded here: ojs/lib/pkp/classes/template/PKPTemplateManager.inc.php 
-				$urlMomentJS = 'https://cdn.jsdelivr.net/momentjs/latest/moment.min.js';
-				$urlDaterangepickerJS = 'https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js';
-				$urlDaterangepickerCSS = 'https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css';
-				$urlLeafletControlGeocodeJS = 'https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js';
-				$urlLeafletControlGeocodeCSS = 'https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css';
-			} else {
-				$urlLeafletCSS = $request->getBaseUrl() . '/' . $this->getPluginPath() . '/enable_cdn_Off/leaflet/leaflet.css';
-				$urlLeafletJS = $request->getBaseUrl() . '/' . $this->getPluginPath() . '/enable_cdn_Off/leaflet/leaflet.js';
-				$urlLeafletDrawCSS = $request->getBaseUrl() . '/' . $this->getPluginPath() . '/enable_cdn_Off/leaflet-draw/leaflet.draw.css';
-				$urlLeafletDrawJS = $request->getBaseUrl() . '/' . $this->getPluginPath() . '/enable_cdn_Off/leaflet-draw/leaflet.draw.js';
-				// $urlJqueryJS = $request->getBaseUrl() . '/' . $this->getPluginPath() . '/enable_cdn_Off/daterangepicker/jquery.min.js';
-				// jquery - no need to load, already loaded here: ojs/lib/pkp/classes/template/PKPTemplateManager.inc.php 
-				$urlMomentJS = $request->getBaseUrl() . '/' . $this->getPluginPath() . '/enable_cdn_Off/daterangepicker/moment.min.js';
-				$urlDaterangepickerJS = $request->getBaseUrl() . '/' . $this->getPluginPath() . '/enable_cdn_Off/daterangepicker/daterangepicker.min.js';
-				$urlDaterangepickerCSS = $request->getBaseUrl() . '/' . $this->getPluginPath() . '/enable_cdn_Off/daterangepicker/daterangepicker.css';
-				$urlLeafletControlGeocodeJS = $request->getBaseUrl() . '/' . $this->getPluginPath() . '/enable_cdn_Off/leaflet-control-geocoder/dist/Control.Geocoder.js';
-				$urlLeafletControlGeocodeCSS = $request->getBaseUrl() . '/' . $this->getPluginPath() . '/enable_cdn_Off/leaflet-control-geocoder/dist/Control.Geocoder.css';
-			}
-			*/
-			/*
-			Here further scripts like JS and CSS are included, 
-			these are included by the following lines and need not be referenced (e.g. in .tbl files).
-			Further information can be found here: https://docs.pkp.sfu.ca/dev/plugin-guide/en/examples-styles-scripts
-			*/
-			
-			/*
-			// loading the leaflet scripts, source: https://leafletjs.com/examples/quick-start/
-			$templateMgr->addStyleSheet('leafletCSS', $urlLeafletCSS, array('contexts' => array('frontend', 'backend')));
-			$templateMgr->addJavaScript('leafletJS', $urlLeafletJS, array('contexts' => array('frontend', 'backend')));
-
-			// loading the leaflet draw scripts, source: https://www.jsdelivr.com/package/npm/leaflet-draw?path=dist
-			$templateMgr->addStyleSheet("leafletDrawCSS", $urlLeafletDrawCSS, array('contexts' => array('frontend', 'backend')));
-			$templateMgr->addJavaScript("leafletDrawJS", $urlLeafletDrawJS, array('contexts' => array('frontend', 'backend')));
-
-			// loading the daterangepicker scripts, source: https://www.daterangepicker.com/#example2 
-			//$templateMgr->addJavaScript("jqueryJS", $urlJqueryJS, array('contexts' => array('frontend', 'backend')));
-			// jquery no need to load, already loaded here: ojs/lib/pkp/classes/template/PKPTemplateManager.inc.php 
-			$templateMgr->addJavaScript("momentJS", $urlMomentJS, array('contexts' => array('frontend', 'backend')));
-			$templateMgr->addJavaScript("daterangepickerJS", $urlDaterangepickerJS, array('contexts' => array('frontend', 'backend')));
-			$templateMgr->addStyleSheet("daterangepickerCSS", $urlDaterangepickerCSS, array('contexts' => array('frontend', 'backend')));
-
-			// loading leaflet control geocoder (search), source: https://github.com/perliedman/leaflet-control-geocoder 
-			$templateMgr->addJavaScript("leafletControlGeocodeJS", $urlLeafletControlGeocodeJS, array('contexts' => array('frontend', 'backend')));
-			$templateMgr->addStyleSheet("leafletControlGeocodeCSS", $urlLeafletControlGeocodeCSS, array('contexts' => array('frontend', 'backend')));
-
-			// main js scripts
-			$templateMgr->assign('submissionMetadataFormFieldsJS', $request->getBaseUrl() . '/' . $this->getPluginPath() . '/js/submissionMetadataFormFields.js');
-			$templateMgr->assign('article_detailsJS', $request->getBaseUrl() . '/' . $this->getPluginPath() . '/js/article_details.js');
-			*/
 		}
 		return $success;
 	}
@@ -258,17 +171,9 @@ class ojsErcPlugin extends GenericPlugin
 		// example: the arrow is used to access the attribute smarty of the variable smarty 
 		// $templateMgr = $smarty->smarty; 
 
-
 		$request = Application::get()->getRequest();
 		$context = $request->getContext();
-
 		
-		//Check if the user has entered an username in the plugin settings for the geonames API (https://www.geonames.org/login). 
-		//The result is passed on accordingly to submissionMetadataFormFields.js as template variable. 
-		
-		//$usernameGeonames = $this->getSetting($context->getId(), 'usernameGeonames');
-		//$templateMgr->assign('usernameGeonames', $usernameGeonames);
-
 		/*
 		In case the user repeats the step "3. Enter Metadata" in the process 'Submit an Article' and comes back to this step to make changes again, 
 		the already entered data is read from the database, added to the template and displayed for the user.
@@ -282,26 +187,9 @@ class ojsErcPlugin extends GenericPlugin
 		$publication = $publicationDao->getById($submissionId);
 
 		$ErcId = $publication->getData('ojsErcPlugin::ErcId');
-		//$spatialProperties = $publication->getData('geoOJS::spatialProperties');
-		//$administrativeUnit = $publication->getData('coverage');
-
-		// for the case that no data is available 
-		//if ($temporalProperties === null) {
-		//	$temporalProperties = 'no data';
-		//}
-
-		//if ($spatialProperties === null || $spatialProperties === '{"type":"FeatureCollection","features":[],"administrativeUnits":{},"temporalProperties":{"unixDateRange":"not available","provenance":{"description":"not available","id":"not available"}}}') {
-		//	$spatialProperties = 'no data';
-		//}
-
-		//if (current($administrativeUnit) === '' || $administrativeUnit === '' || $administrativeUnit === null) {
-		//	$administrativeUnit = 'no data';
-		//}
-
+		
 		//assign data as variables to the template 
 		$templateMgr->assign('ErcIdFromDb', $ErcId);
-		//$templateMgr->assign('spatialPropertiesFromDb', $spatialProperties);
-		//$templateMgr->assign('administrativeUnitFromDb', $administrativeUnit);
 
 		// echo "TestTesTest123"; // by echo a direct output is created on the page
 
@@ -310,69 +198,6 @@ class ojsErcPlugin extends GenericPlugin
 
 		return false;
 	}
-
-	/**
-	 * Function which extends ArticleMain Template by geospatial properties if available. 
-	 * Data is loaded from the database, passed as template variable to the 'article_details.tpl' 
-	 * and requested from there in the 'article_details.js' to display coordinates in a map, the date and coverage information if available.
-	 * @param hook Templates::Article::Main
-	 */
-	
-	public function extendArticleMainTemplate($hookName, $params)
-	{
-		$newPublication = $params[0];
-		$params = $params[2];
-		/*
-		$templateMgr = &$params[1];
-		$output = &$params[2];
-
-		$publication = $templateMgr->getTemplateVars('publication');
-		$submission = $templateMgr->getTemplateVars('article');
-		$submissionId = $submission->getId();
-
-		// get data from database 
-		$temporalProperties = $publication->getData('geoOJS::temporalProperties');
-		$spatialProperties = $publication->getData('geoOJS::spatialProperties');
-		$administrativeUnit = $publication->getData('coverage');
-
-		// for the case that no data is available 
-		if ($temporalProperties === null || $temporalProperties === '') {
-			$temporalProperties = 'no data';
-		}
-
-		if (($spatialProperties === null || $spatialProperties === '{"type":"FeatureCollection","features":[],"administrativeUnits":{},"temporalProperties":{"unixDateRange":"not available","provenance":"not available"}}')) {
-			$spatialProperties = 'no data';
-		}
-
-		if (current($administrativeUnit) === '' || $administrativeUnit === '') {
-			$administrativeUnit = 'no data';
-		}
-
-		//assign data as variables to the template 
-		$templateMgr->assign('temporalProperties', $temporalProperties);
-		$templateMgr->assign('spatialProperties', $spatialProperties);
-		$templateMgr->assign('administrativeUnit', $administrativeUnit);
-
-		$output .= $templateMgr->fetch($this->getTemplateResource('frontend/objects/article_details.tpl'));
-
-		return false;
-		*/
-	}
-
-	/**
-	 * Function which extends the ArticleMain Template by a download button for the geospatial Metadata as geoJSON. 
-	 * @param hook Templates::Article::Details
-	 */
-	/*
-	public function extendArticleDetailsTemplate($hookName, $params)
-	{
-		$templateMgr = &$params[1];
-		$output = &$params[2];
-
-		$output .= $templateMgr->fetch($this->getTemplateResource('frontend/objects/article_details_download.tpl'));
-
-		return false;
-	}*/
 
 	/**
 	 * Function which extends the schema of the publication_settings table in the database. 
@@ -394,19 +219,6 @@ class ojsErcPlugin extends GenericPlugin
 		}';
 		$ErcIdDecoded = json_decode($ErcId);
 		$schema->properties->{'ojsErcPlugin::ErcId'} = $ErcIdDecoded;
-
-		/*
-		$spatialProperties = '{
-			"type": "string",
-			"multilingual": false,
-			"apiSummary": true,
-			"validation": [
-				"nullable"
-			]
-		}';
-		$spatialPropertiesDecoded = json_decode($spatialProperties);
-		$schema->properties->{'geoOJS::spatialProperties'} = $spatialPropertiesDecoded;
-		*/
 	}
 
 	/**
@@ -421,43 +233,13 @@ class ojsErcPlugin extends GenericPlugin
 		$newPublication = $params[0];
 		$params = $params[2];
 
-		$temporalProperties = $_POST['temporalProperties'];
-		$spatialProperties = $_POST['spatialProperties'];
-		$administrativeUnit = $_POST['administrativeUnit'];
-
 		$ErcId = $_POST['ErcId'];
-
-		$exampleTimestamp = '2020-08-12 11:00 AM - 2020-08-13 07:00 PM';
-		$exampleSpatialProperties = '{"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[7.516193389892579,51.94553466305084],[7.516193389892579,51.96447134091556],[7.56511688232422,51.96447134091556],[7.56511688232422,51.94553466305084],[7.516193389892579,51.94553466305084]]]},"properties":{"name":"TODO Administrative Unit"}}]}';
-		$exampleCoverageElement = 'TODO';
 
 		$testErcId = 'geQfc'; 
 
 		/*
-		If the element to store in the database is an element which is different in different languages 
-		the property "multilingual" in the function addToSchema has to be true, and you have to use a loop like this 
-
-		$localePare = $params['title'];
-
-		foreach ($localePare as $localeKey => $fileId) {
-			$newPublication->setData('jatsParser::fullText', $htmlDocument->saveAsHTML(), $localeKey);
-		}
-
-		further information: https://github.com/Vitaliy-1/JATSParserPlugin/blob/21425c486f0f157cd8dc6b829322cd32159dd408/JatsParserPlugin.inc.php#L619 
-
-		For elements which are not multilangual you can skip the parameter $localeKey and just do it like this: 
-			$newPublication->setData('geoOJS::spatialProperties', $spatialProperties);
-
-		Take care, function is called twice, first during Submission Workflow and also before Schedule for Publication in the Review Workflow!!!
-		*/
-
-		// null if there is no possibility to input data (metadata input before Schedule for Publication)
-		
-		//$newPublication->setData('galleys', 'test');
-
-
-		/*
 		If the user entered an ErcId, an html with the id in it is created, this html is uploaded as file and submission file and attachted to an galley. 
+		Take care, function is called twice, first during Submission Workflow and also before Schedule for Publication in the Review Workflow!!!
 		*/
 		if ($ErcId !== null) {
 			$newPublication->setData('ojsErcPlugin::ErcId', $ErcId);
@@ -466,7 +248,6 @@ class ojsErcPlugin extends GenericPlugin
 			$pathHtmlFile = $this->getPluginPath() . '/ERCGalleyInitial.html'; 
 			
 			$ErcHtmlFileName = 'ERCGalley-' . $ErcId . '.html'; 
-
 			
 			/*
 			Update the index.html of the build in terms of the ErcId and store it at a temporary location  
@@ -548,106 +329,15 @@ class ojsErcPlugin extends GenericPlugin
 			$articleGalleyDAO = DAORegistry::getDAO('ArticleGalleyDAO');
 			$galley = $articleGalleyDAO->newDataObject();
 
-			//$galley->setSubmissionFileId(3); 
 			$galley->setData('submissionFileId', $submissionFileId); 
 			$galley->setLocale('en_US');
-			//$galley->setId($id);
 			$galley->setData('publicationId', $submissionId);
-			//$galley->setPublication($newPublication);
 			$galley->setLabel('ERC');
-			//$galley->setData('submission_file_id', 'erstesERC');
 			$galley->setSequence(1);
 			$galley->setData('urlPath', 'erc-' . $ErcId);
-			//$galley->setDataObjectId($id);
-			//$articleTombstone->stampDateDeleted();
-			//$articleTombstone->setSetSpec($setSpec);
-			//$articleTombstone->setSetName($section->getLocalizedTitle());
-			//$articleTombstone->setOAIIdentifier($oaiIdentifier);
-			//$articleTombstone->setOAISetObjectsIds($OAISetObjectsIds);
 			$articleGalleyDAO->insertObject($galley);
-			//$reviewRoundDao->insertObject($reviewRound);
-		}
-
-
-		/*
-		if ($temporalProperties !== null && $temporalProperties !== "") {
-			$newPublication->setData('geoOJS::temporalProperties', $temporalProperties);
-		}
-
-		if ($administrativeUnit !== null) {
-			$newPublication->setData('coverage', $administrativeUnit);
-		}*/
-
-		/*
-		The following lines are probably needed if you want to store text in a certain language to set the local key,
-		further information can be found here:
-		https://github.com/Vitaliy-1/JATSParserPlugin/blob/21425c486f0f157cd8dc6b829322cd32159dd408/JatsParserPlugin.inc.php#L619 
-		
-		$yourdata = 100;
-		$yourdata2 = '00:00:00';
-		$yourdata3 = 'TesTestTest';
-		$localePare = $params['title'];
-		foreach ($localePare as $localeKey => $fileId) {
-			continue;
-		}
-		$newPublication->setData('Textfeld', $yourdata3, $localeKey);
-		*/
-	}
-
-	/**
-	 * Not working function to edit a form before Schedule for Publication. 
-	 * Possible solution can be found here: https://forum.pkp.sfu.ca/t/insert-in-submission-settings-table/61291/19?u=tnier01, 
-	 * which is already implemented partly here, but commented out! 
-	 */
-	public function extendScheduleForPublication(string $hookName, FormComponent $form): void
-	{
-
-		// Import the FORM_METADATA constant
-		import('lib.pkp.classes.components.forms.publication.PKPMetadataForm');
-
-		if ($form->id !== 'metadata' || !empty($form->errors)) return;
-
-		if ($form->id === 'metadata') {
-
-			/*
-			$publication = Services::get('publication');
-			$temporalProperties = $publication->getData('geoOJS::temporalProperties');
-			$spatialProperties = $publication->getData('geoOJS::spatialProperties');
-			*/
-
-			/*$form->addField(new \PKP\components\forms\FieldOptions('jatsParser::references', [
-				'label' => 'Hello',
-				'description' => 'Hello',
-				'type' => 'radio',
-				'options' => null,
-				'value' => null
-			]));*/
-
-			// Add a plain HTML field to the form
-			/*$form->addField(new FieldHTML('myFieldName', [
-				'label' => 'My Field Name',
-				'description' => '<p>Add any HTML code that you want.</p>
-				<div id="mapdiv" style="width: 1116px; height: 400px; float: left;  z-index: 0;"></div>
-				<script src="{$submissionMetadataFormFieldsJS}" type="text/javascript" defer></script>',
-			]));*/
 		}
 	}
-
-	/**
-	 * Not working function to edit a form before Schedule for Publication. 
-	 */
-	/*
-	public function extendScheduleForPublication2($hookName, $params)
-	{
-		$templateMgr = &$params[1];
-		$output = &$params[2];
-
-		// echo "<p> Hello </p>";
-
-		// $output .= $templateMgr->fetch($this->getTemplateResource('frontend/objects/article_details.tpl'));
-
-		return false;
-	}*/
 
 	/**
 	 * @copydoc Plugin::getActions() - https://docs.pkp.sfu.ca/dev/plugin-guide/en/settings
